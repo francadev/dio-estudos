df5df01e83abf956e95aa6e46461e09c36e7861e
Rafael <francadev@hotmail.com>
2024-06-12 19:55:40 -0300

    feat: excluindo venv

--- Python_AI_Backend_Developer/12_banco_de_dados/script.py
+++ Python_AI_Backend_Developer/12_banco_de_dados/script.py
@@ -1,1 +1,19 @@
-print("Oi")
+import psycopg2
+import psycopg2.extras
+
+db = psycopg2.connect("dbname=dio_estudos user=postgres password=")
+cur = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cur.execute("SELECT * FROM usuarios WHERE id = 1")
+
+# procura por todos
+records = cur.fetchall()
+
+for record in records:
+    print(dict(record))
+
+# procura por dois registros
+# records = cur.fetchmany(size=2)
+# print(records)
+
+db.commit()
+cur.close()
